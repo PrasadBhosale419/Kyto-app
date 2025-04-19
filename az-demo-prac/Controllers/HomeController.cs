13eda3c9a6cffb95a6cf4d1364298f27910e3c09
PrasadBhosale419 <prasadbhosale834@example.com>
2025-04-19 13:40:19 +0530

    Getting started

---
+++ az-demo-prac/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using az_demo_prac.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace az_demo_prac.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ az-demo-prac/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace az_demo_prac.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
